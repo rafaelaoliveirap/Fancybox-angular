7e32db2e5a7e6e026f7a4e193920dac6763d3e3a
Jeferson Monteiro <jefimpjjn@gmail.com>
2020-06-27 15:06:07 -0300

    Alteração no Responsivo do Depoimentos

--- fancyBox/src/app/home/home.component.ts
+++ fancyBox/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  faArrowRight = faArrowRight
+  faArrowLeft = faArrowLeft
 
   ngOnInit() {
     window.scroll(0, 0)
